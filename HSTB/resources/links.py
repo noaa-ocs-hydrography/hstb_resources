4507cb7a3678c45730a7bee9b319361b00e02cc5
barry-gallagher <barry.gallagher@noaa.gov>
2021-03-04 10:37:44 -0500

    initial commit

---
+++ HSTB/resources/links.py
@@ -0,0 +1,3 @@
+epsg_descriptions = "https://spatialreference.org/ref/epsg/"
+pydro_download = "https://nauticalcharts.noaa.gov/data/tools-apps.html"
+pydro_help = "https://svn.pydro.noaa.gov/"
---
+++ README.md
@@ -0,0 +1,2 @@
+# shared
+Collection of supporting images, icons, addresses, forms for all modules
